d238b9eaea571d781bb6b2ab80e76e39a8fca778
sofiagakalo1 <108238510+sofiagakalo1@users.noreply.github.com>
2022-11-21 23:24:51 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,58 @@
 // HTML містить розмітку форми.Напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
 
 import throttle from 'lodash.throttle';
-import { save, load, remove } from './storage';
 
-const form = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
-//отримуємо і читаємо значення ключа
-let storage = load(storageKey);
-//об'єкт значень з форми
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+//key can be as const
+// const storageKey = 'feedback-form-state';
+
+refs.form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+fromLocalStorage();
+
+
 let formData = {};
 
-form.addEventListener('input', throttle(onInputClick, 500));
-form.addEventListener('submit', onSubmitClick);
-
-function onInputClick(event) {
+function onInputForm(event) {
   //отримуємо данні
   formData[event.target.name] = event.target.value;
   //записуємо данні у сховище
-  save(storageKey, formData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function onSubmitClick(event) {
+function onSubmitForm(event) {
   //відміняємо перезагрузку
   event.preventDefault();
-  //присвоюємо елементи події
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+ 
   //перевіряємо заповненість форми
-  if (email.value === '' || message.value === '') {
+  if (refs.input.value === '' || refs.textarea.value === '') {
     return alert('Заповніть всі поля форми');
   }
   //Виводимо в консоль об'єкт
-  console.log({ Email: email.value, Message: message.value });
+  console.log(localStorage.getItem('feedback-form-state'));
+  //очищуємо поля форми
   event.currentTarget.reset();
-  remove(storageKey);
+  localStorage.removeItem('feedback-form-state');
 }
 
-function localStorage(data) {
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+function fromLocalStorage() {
+  //дістаємо зі сховища попередні данні форми
+  const savedFormData = localStorage.getItem('feedback-form-state');
+  //якщо у сховищі є збережені данні, то розпаковуємо їх і виводимо у форму
+  if (savedFormData) {
+    const data = JSON.parse(savedFormData);
+    if (data.email) {
+      refs.input.value = data.email;
+    }
+    if (data.message) {
+      refs.textarea.value = data.message;
+    }
   }
 }
-localStorage();
 
 // Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message,
 //  у яких зберігай поточні значення полів форми.Нехай ключем для сховища буде рядок "feedback-form-state".
